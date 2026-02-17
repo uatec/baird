e24ab1352f39e7be273d280bd0641f71a4d95fc3
Robert Stiff <uatecuk@gmail.com>
2026-02-17 23:09:12 +0000

    cec debugging improvements

--- Baird/Services/ICecService.cs
+++ Baird/Services/ICecService.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Threading.Tasks;
 
 namespace Baird.Services
 {
+    public class CecCommandLoggedEventArgs : EventArgs
+    {
+        public string Command { get; set; } = string.Empty;
+        public string Response { get; set; } = string.Empty;
+        public bool Success { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.Now;
+    }
+
     public interface ICecService
     {
+        event EventHandler<CecCommandLoggedEventArgs>? CommandLogged;
+        
         Task StartAsync();
         Task TogglePowerAsync();
         Task PowerOnAsync();
